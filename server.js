168d975b2fca3664c15ccca3f8273db3c5f6fd7c
rich kevan <rskevan@me.com>
2021-12-08 14:23:16 -0800

    update port

--- server.js
+++ server.js
@@ -3,13 +3,15 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const port = process.env.PORT || 3000;
+app.use(cors()); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -28,6 +30,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(port, function () {
+  console.log(`Your app is listening on port ${port}`);
 });
